5701024513da7a1269329144a88bd618ab3d9f45
Romain Vial <romain.vial@mines-paristech.fr>
2018-02-21 16:31:07 +0100

    datasets should be treated separately + move feature computation outside of the Dataset class

--- dataset.py
+++ dataset.py
@@ -7,9 +7,9 @@ class Dataset:
     def __init__(self, train_sequence_files, train_feature_files, train_label_files,
                  test_sequence_files, test_feature_file, val_ratio=0.2):
         self.dataset = {
-            'train': {'sequences': [], 'labels': [], 'features': [], 'basic_features': []},
-            'val': {'sequences': [], 'labels': [], 'features': [], 'basic_features': []},
-            'test': {'sequences': [], 'labels': [], 'features': [], 'basic_features': []}
+            'train': {'sequences': [], 'labels': [], 'basic_features': []},
+            'val': {'sequences': [], 'labels': [], 'basic_features': []},
+            'test': {'sequences': [], 'labels': [], 'basic_features': []}
         }
 
         for file_path in train_sequence_files:
@@ -28,9 +28,6 @@ class Dataset:
             self._read_X_mat_csv(file_path, 'test')
 
         self._compute_val_split(val_ratio)
-        self._compute_features('train')
-        self._compute_features('val')
-        self._compute_features('test')
 
     def _read_X_csv(self, csv_path, split):
         with open(csv_path) as csv_file:
@@ -67,25 +64,13 @@ class Dataset:
         self.dataset['train']['basic_features'] = [split_copy['basic_features'][idx] for idx in train_indexes]
         self.dataset['train']['labels'] = [split_copy['labels'][idx] for idx in train_indexes]
 
-    def _compute_features(self, split):
-        # The critical part
-        features = []
-        char_dict = {'A': [1, 0, 0, 0], 'T': [0, 1, 0, 0], 'C': [0, 0, 1, 0], 'G': [0, 0, 0, 1]}
-        for sequence in self.dataset[split]['sequences']:
-            sequence_feature = []
-            for char in sequence:
-                sequence_feature += char_dict[char]
-            features.append(sequence_feature)
-
-        self.dataset[split]['features'] = features
-
 
 if __name__ == "__main__":
-    dataset = Dataset(['data/Xtr0.csv', 'data/Xtr1.csv', 'data/Xtr2.csv'],
-                      ['data/Xtr0_mat50.csv', 'data/Xtr1_mat50.csv', 'data/Xtr2_mat50.csv'],
-                      ['data/Ytr0.csv', 'data/Ytr1.csv', 'data/Ytr2.csv'],
-                      ['data/Xte0.csv', 'data/Xte1.csv', 'data/Xte2.csv'],
-                      ['data/Xte0_mat50.csv', 'data/Xte1_mat50.csv', 'data/Xte2_mat50.csv'])
+    dataset = Dataset(['data/Xtr0.csv'],
+                      ['data/Xtr0_mat50.csv'],
+                      ['data/Ytr0.csv'],
+                      ['data/Xte0.csv'],
+                      ['data/Xte0_mat50.csv'])
 
     print np.bincount(dataset.dataset['train']['labels']).astype(float) / len(dataset.dataset['train']['labels'])
     print np.bincount(dataset.dataset['val']['labels']).astype(float) / len(dataset.dataset['val']['labels'])
