91ff9c82d0782960721391d0160c0c4ab42cab3a
Mohammed Chlieh <mohammed.chlieh@gmail.com>
2018-02-28 14:42:33 +0100

    svm primal implementation

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import numpy as np
+import time
 from sklearn.svm import SVC
 from dataset import Dataset
 from kernels import compute_kernel_matrix
+from svm import MySVM
+import os
 
+# dataset 0, spectrum_size = 5
+# dataset 1, spectrum_size = 4
+# dataset 2, spectrum_size = 3
 dataset_id = 1
 dataset = Dataset(['data/Xtr{}.csv'.format(dataset_id)],
                   ['data/Xtr{}_mat50.csv'.format(dataset_id)],
@@ -19,14 +25,42 @@ print 'val 0/1 proportion:', (np.bincount(dataset.dataset['val']['labels']).asty
 spectrum_size = 4
 feature_extractor = 'mismatch'
 normalization = True
-K_train = compute_kernel_matrix(dataset.dataset['train']['sequences'],
-                                spectrum_size=spectrum_size,
-                                feature_extractor=feature_extractor, normalization=normalization)
-K_val = compute_kernel_matrix(dataset.dataset['val']['sequences'], dataset.dataset['train']['sequences'],
-                              spectrum_size=spectrum_size,
-                              feature_extractor=feature_extractor, normalization=normalization)
+
+print "Computing Kernels..."
+K_train_path = './K_train_dataset_%s.npy' % dataset_id
+K_val_path = './K_val_dataset_%s.npy' % dataset_id
+
+if os.path.exists(K_val_path) and os.path.exists(K_train_path):
+    K_val = np.load(K_val_path)
+    K_train = np.load(K_train_path)
+else:
+    K_train = compute_kernel_matrix(dataset.dataset['train']['sequences'],
+                                    spectrum_size=spectrum_size,
+                                    feature_extractor=feature_extractor, normalization=normalization)
+    K_val = compute_kernel_matrix(dataset.dataset['val']['sequences'], dataset.dataset['train']['sequences'],
+                                  spectrum_size=spectrum_size,
+                                  feature_extractor=feature_extractor, normalization=normalization)
+    np.save(K_train_path, K_train)
+    np.save(K_val_path, K_val)
 
 svc = SVC(kernel='precomputed', C=10.)
+my_svm = MySVM(C=10., dual=False, verbose=True)
+
+print "Fitting sklearn model..."
+start = time.time()
 svc.fit(K_train, dataset.dataset['train']['labels'])
+print "Fitting took %.3f seconds" % (time.time() - start)
+
+print "Fitting our model..."
+start = time.time()
+my_svm.fit(K_train, dataset.dataset['train']['labels'])
+print "Fitting took %.3f seconds" % (time.time() - start)
+
+
+print "Scoring with sklearn model:"
 print svc.score(K_train, dataset.dataset['train']['labels'])
 print svc.score(K_val, dataset.dataset['val']['labels'])
+
+print "Scoring with our model:"
+print my_svm.score(K_train, dataset.dataset['train']['labels'])
+print my_svm.score(K_val, dataset.dataset['val']['labels'])
---
+++ svm.py
@@ -0,0 +1,92 @@
+import cvxopt
+from cvxopt.base import matrix
+import numpy as np
+
+
+def cvxopt_solve_qp(P, q, G=None, h=None, A=None, b=None):
+    """
+    Wrapper for CVXOPT solver:
+        min [ 1/2 (x^T P x) + q^T x ]
+            s.t G x <= h and A x = b
+    :return: ndarray solution
+    """
+    args = [matrix(P), matrix(q)]
+    if G is not None:
+        args.extend([matrix(G), matrix(h)])
+        if A is not None:
+            args.extend([matrix(A), matrix(b)])
+    sol = cvxopt.solvers.qp(*args, kktsolver='ldl', options={'kktreg': 1e-9})
+    if 'optimal' not in sol['status']:
+        return None
+    return np.array(sol['x']).reshape((P.shape[1],))
+
+
+class MySVM:
+    def __init__(self, C, dual=True, verbose=False):
+        self.C = C
+        self.dual = dual
+        self.alpha = None
+        self.verbose = verbose
+
+    def solve_primal_svm(self, K, y):
+        """
+        The primal problem is:
+            min_{alpha, xi} [ 1 / n sum_i {xi_i} + lambda * alpha.T K alpha ]
+                s.t y_i [K alpha]_i + xi_i - 1 >= 0 for i = 1..n
+                    and x_i >= 0 for i = 1..n
+        :param K: np array of shape (n, n) - Kernel matrix
+        :param y: 1-d array labels
+        :return: 1-d array solution [alpha, xi]
+        """
+        n = K.shape[0]
+        lambda_ = 1. / (2. * n * self.C)
+
+        # computing P
+        temp = np.hstack([K, np.zeros((n, n))])
+        P = 2. * lambda_ * np.vstack([temp, np.zeros((n, 2 * n))])
+
+        q = 1. / n * np.vstack([np.zeros((n, 1)), np.ones((n, 1))])
+        # computing G
+        temp1 = np.hstack([- y.reshape((n, 1)) * K, - np.identity(n)])
+        temp2 = np.hstack([np.zeros((n, n)), - np.identity(n)])
+        G = np.vstack([temp1, temp2])
+        h = -1. * np.vstack([np.ones((n, 1)), np.zeros((n, 1))])
+        return cvxopt_solve_qp(P=P, q=q, G=G, h=h)
+
+    def fit(self, K, y):
+        assert len(set(y)) == 2, "This class only supports binary classification."
+        assert set(y) == {0, 1}, "Please transform labels into (0, 1) values."
+
+        y_copy = np.array(y)
+        for i, class_ in enumerate(y):
+            if class_ == 0:
+                y_copy[i] = -1
+        if not self.dual:
+            self.alpha, _ = np.split(self.solve_primal_svm(K, y_copy), 2)
+        else:
+            self.alpha = None
+
+    def predict(self, K_val):
+        y_pred = np.zeros((K_val.shape[0],))
+        f = K_val.dot(self.alpha)
+        for i, value in enumerate(f):
+            if value > 0.:
+                y_pred[i] = 1
+        return y_pred
+
+    def score(self, K_val, y_true):
+        """
+
+        :param K_val: This is the kernel matrix between Validation features and Training features
+        :param y_true: true labels
+        :return: Accuracy score
+        """
+        score = 0
+        y_pred = self.predict(K_val)
+        n = len(y_true)
+        assert n == y_pred.shape[0], "Something is wrong, y_true and y_pred have different lengths"
+        for i in xrange(n):
+            if y_pred[i] == y_true[i]:
+                score += 1
+
+        return score / float(n)
