8a7e62d199c7bff155c37d4be2b6846c8fb2b8ec
Romain Vial <romain.vial@mines-paristech.fr>
2018-02-28 12:07:47 +0100

    add different spectrum size

--- kernels.py
+++ kernels.py
@@ -1,49 +1,68 @@
+import itertools
 import numpy as np
 
 alphabet = ['A', 'T', 'C', 'G']
-spectrum = [l1 + l2 + l3 for l1 in alphabet for l2 in alphabet for l3 in alphabet]
 
 
-def spectrum_feature(X):
+def compute_spectrum(spectrum_size):
+    spectrum = []
+    for element in itertools.product(alphabet, repeat=spectrum_size):
+        spectrum.append(''.join(element))
+    return spectrum
+
+
+def spectrum_feature(X, spectrum):
+    spectrum_size = len(spectrum[0])
     nb_samples = len(X)
     features = np.zeros((nb_samples, len(spectrum)))
     for i in range(nb_samples):
-        for j in range(len(X[i])-2):
-            features[i, spectrum.index(X[i][j:j + 3])] += 1
+        for j in range(len(X[i]) - spectrum_size + 1):
+            features[i, spectrum.index(X[i][j:j + spectrum_size])] += 1
     return features
 
 
-def mismatch_feature(X):
+def mismatch_feature(X, spectrum):
+    spectrum_size = len(spectrum[0])
     nb_samples = len(X)
     features = np.zeros((nb_samples, len(spectrum)))
     for i in range(nb_samples):
-        for j in range(len(X[i]) - 2):
-            mismatches = set([l + X[i][j+1:j+3] for l in alphabet] +
-                             [X[i][j] + l + X[i][j+2] for l in alphabet] +
-                             [X[i][j:j+2] + l for l in alphabet])
+        for j in range(len(X[i]) - spectrum_size + 1):
+            substring = X[i][j:j+spectrum_size]
+
+            mismatches = []
+            for k in range(spectrum_size):
+                mismatches += [substring[:k] + l + substring[k+1:] for l in alphabet]
+            mismatches = set(mismatches)
+
             for mismatch in mismatches:
                 features[i, spectrum.index(mismatch)] += 1
     return features
 
 
-def compute_kernel_matrix(X1, X2=None, feature_extractor='spectrum', normalize=True):
+def normalize(K, features1, features2=None):
+    if features2 is not None:
+        k1 = np.expand_dims(1. / np.sqrt(np.sum(features1 ** 2, axis=1)), axis=1)
+        k2 = np.expand_dims(1. / np.sqrt(np.sum(features2 ** 2, axis=1)), axis=1)
+        return K * k1.dot(k2.T) + 1.
+    else:
+        k = np.expand_dims(1. / np.sqrt(np.diagonal(K)), axis=1)
+        return K * k.dot(k.T) + 1.
+
+
+def compute_kernel_matrix(X1, X2=None, spectrum_size=3, feature_extractor='spectrum', normalization=True):
+    spectrum = compute_spectrum(spectrum_size)
     feature_fun = get_feature_extractor(feature_extractor)
-    features1 = feature_fun(X1)
+    features1 = feature_fun(X1, spectrum)
+    features2 = None
 
     if X2 is not None:
-        features2 = feature_fun(X2)
+        features2 = feature_fun(X2, spectrum)
         K = features1.dot(features2.T)
-
-        if normalize:
-            k1 = np.expand_dims(1. / np.sqrt(np.sum(features1 ** 2, axis=1)), axis=1)
-            k2 = np.expand_dims(1. / np.sqrt(np.sum(features2 ** 2, axis=1)), axis=1)
-            K = K * k1.dot(k2.T) + 1.
     else:
         K = features1.dot(features1.T)
 
-        if normalize:
-            k = np.expand_dims(1. / np.sqrt(np.diagonal(K)), axis=1)
-            K = K * k.dot(k.T) + 1.
+    if normalization:
+        K = normalize(K, features1, features2)
 
     return K
 
@@ -53,3 +72,10 @@ def get_feature_extractor(name):
         return spectrum_feature
     elif name == 'mismatch':
         return mismatch_feature
+
+
+if __name__ == "__main__":
+    seq1 = 'CGGGCCTCCTCCAGGCTCAGAATCGACCCCCCCCCATCCTGATAGACCACAACGAAAGCCGTAGCGACGGCCGCAGGAGCTGGCGCGACAGCCCAGAGCTC'
+    seq2 = 'GGTGTCTAGTCACATTACCTGAGATCTGACATTTTTATCCGTCTGAGCACTTGATGGTGATTTCTTTCACTGTAGTCAAGTTTCTGCCTGTATAGCAGTGG'
+    spec = compute_spectrum(3)
+    print mismatch_feature([seq1], spec)
