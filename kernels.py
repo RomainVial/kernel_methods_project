c94fd5113e43cb9540ef4cd59d515e6250892907
Romain Vial <romain.vial@mines-paristech.fr>
2018-03-02 16:29:43 +0100

    add rbf kernel

--- kernels.py
+++ kernels.py
@@ -49,20 +49,47 @@ def normalize(K, features1, features2=None):
         return K * k.dot(k.T) + 1.
 
 
-def compute_kernel_matrix(X1, X2=None, spectrum_size=3, feature_extractor='spectrum', normalization=True):
+def linear_kernel(features1, features2=None, **kwargs):
+    if features2 is not None:
+        K = features1.dot(features2.T)
+    else:
+        K = features1.dot(features1.T)
+
+    if kwargs['normalization']:
+        K = normalize(K, features1, features2)
+
+    return K
+
+
+def rbf_kernel(features1, features2=None, **kwargs):
+    if 'gamma' in kwargs:
+        gamma = kwargs['gamma']
+    else:
+        gamma = 1. / features1.shape[1]
+
+    XX = np.linalg.norm(features1, axis=1)[:, np.newaxis] ** 2
+
+    if features2 is not None:
+        YY = np.linalg.norm(features2, axis=1)[:, np.newaxis] ** 2
+        distances = XX - 2 * features1.dot(features2.T) + YY.T
+    else:
+        distances = XX - 2 * features1.dot(features1.T) + XX.T
+
+    K = np.exp(-gamma * distances)
+    return K
+
+
+def compute_kernel_matrix(X1, X2=None, spectrum_size=3, feature_extractor='spectrum', kernel='linear', **kwargs):
     spectrum = compute_spectrum(spectrum_size)
     feature_fun = get_feature_extractor(feature_extractor)
+    kernel_fun = get_kernel(kernel)
     features1 = feature_fun(X1, spectrum)
     features2 = None
 
     if X2 is not None:
         features2 = feature_fun(X2, spectrum)
-        K = features1.dot(features2.T)
-    else:
-        K = features1.dot(features1.T)
 
-    if normalization:
-        K = normalize(K, features1, features2)
+    K = kernel_fun(features1, features2, **kwargs)
 
     return K
 
@@ -72,6 +99,13 @@ def get_feature_extractor(name):
         return spectrum_feature
     elif name == 'mismatch':
         return mismatch_feature
+
+
+def get_kernel(name):
+    if name == 'linear':
+        return linear_kernel
+    elif name == 'rbf':
+        return rbf_kernel
 
 
 if __name__ == "__main__":
